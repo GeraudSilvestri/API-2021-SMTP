dc691adc3b0f17a3f9d2797e76c38a66cc725589
loicrheig <loicrossetmp@gmail.com>
2021-11-10 11:53:12 +0100

    Initial commit

---
+++ FileHandler/src/main/java/FileHandler.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.net.Socket;
+
+/**
+ * Calculator client implementation
+ */
+public class FileHandler {
+
+    private static final Logger LOG = Logger.getLogger(FileHandler.class.getName());
+
+    /**
+     * Main function to run client
+     *
+     * @param args no args required
+     */
+    public static void main(String[] args) {
+
+    }
+}
