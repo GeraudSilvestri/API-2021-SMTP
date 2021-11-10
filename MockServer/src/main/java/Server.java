11d6a763e68b4f97aafd83ac06ffd0b61e44fcb6
Géraud Silvestri <silvestri.geraud@gmail.com>
2021-11-10 11:55:19 +0100

    bonne bases

--- MockServer/src/main/java/Server.java
+++ MockServer/src/main/java/Server.java
@@ -1,5 +1,3 @@
-package ch.heigvd.api.calc;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,12 +10,13 @@ import java.util.logging.Logger;
 public class Server {
 
     private final static Logger LOG = Logger.getLogger(Server.class.getName());
-    int port = 4269;
+    final int PORT = 4269;
+    final String CRLF = "\r\n";
+
     BufferedReader in = null;
     BufferedWriter out = null;
     ServerSocket serverSocket;
     Socket clientSocket = null;
-    String regex = "^(\\d+[\\+\\-\\*\\/]{1})+\\d+$";
 
     /**
      * Main function to start the server
@@ -34,40 +33,24 @@ public class Server {
      */
     private void start() {
         try {
-            serverSocket = new ServerSocket(port);
+            serverSocket = new ServerSocket(PORT);
         } catch (IOException ex) {
             LOG.log(Level.SEVERE, null, ex);
             return;
         }
         while (true) {
             try {
-                LOG.log(Level.INFO, "Single-threaded: Waiting for a new client on port {0}", port);
+                LOG.log(Level.INFO, "Single-threaded: Waiting for a new client on port {0}", PORT);
                 clientSocket = serverSocket.accept();
 
                 handleClient(clientSocket);
                 String line;
             }
             catch(Exception ex){
-                if (in != null) {
-                    try {
-                        in.close();
-                    } catch (IOException ex1) {
-                        LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
-                    }
-                }
-                if (out != null) {
-                    try {
-                        out.close();
-                    } catch (IOException ex1) {
-                        LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
-                    }
-                }
-                if (clientSocket != null) {
-                    try {
-                        clientSocket.close();
-                    } catch (IOException ex1) {
-                        LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
-                    }
+                try{
+                    exit();
+                }catch(IOException e){
+                    LOG.log(Level.SEVERE, e.getMessage(), e);
                 }
                 LOG.log(Level.SEVERE, ex.getMessage(), ex);
             }
@@ -83,7 +66,7 @@ public class Server {
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
         out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
 
-        out.write("connexion etablie...\nOPE 1+2 pour une operation\nEXI pour quitter l'application\n");
+        out.write("connexion etablie..." + CRLF);
         out.flush();
 
         String line;
@@ -93,52 +76,39 @@ public class Server {
             }
 
             switch(line.substring(0,3)){
-                case "OPE":
-                    Integer result = calc(line.substring(4));
-                    out.write(result == -1 ? "CAE : calcul error\n" : result.toString());
-                    out.flush();
-                    break;
-                case "EXI":
-                    out.write("EXS : cleaning resources...\n");
-                    out.flush();
-                    exit();
-                    break;
+
                 default:
-                    out.write("COE : command error\n");
+                    out.write("COE : command error" + CRLF);
                     out.flush();
                     break;
             }
         }
-    }
-
-    /**
-     * do the thing
-     * @param a the thing
-     * @return result of the thing
-     */
-    private int calc(String a){
-        if(a.matches(regex)) {
-            String[] operators = a.split("[0-9]+");
-            String[] operands = a.split("[+-]");
-            int agregate = Integer.parseInt(operands[0]);
-
-            for (int i = 1; i < operands.length; i++) {
-                if (operators[i].equals("+"))
-                    agregate += Integer.parseInt(operands[i]);
-                else
-                    agregate -= Integer.parseInt(operands[i]);
-            }
-            return agregate;
-        }
-        return -1;
     }
 
     /**
      * commits hara-kiri
      */
     private void exit() throws IOException{
-        clientSocket.close();
-        in.close();
-        out.close();
+        if (in != null) {
+            try {
+                in.close();
+            } catch (IOException ex1) {
+                throw ex1;
+            }
+        }
+        if (out != null) {
+            try {
+                out.close();
+            } catch (IOException ex1) {
+                throw ex1;
+            }
+        }
+        if (clientSocket != null) {
+            try {
+                clientSocket.close();
+            } catch (IOException ex1) {
+                throw ex1;
+            }
+        }
     }
 }
