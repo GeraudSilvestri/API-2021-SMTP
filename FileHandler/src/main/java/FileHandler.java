b45bcd42fb0ce25fa30eba4893a3ec2f8a9aa2e8
loicrheig <loicrossetmp@gmail.com>
2021-11-17 10:55:06 +0100

    Begin file lecture

--- FileHandler/src/main/java/FileHandler.java
+++ FileHandler/src/main/java/FileHandler.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.net.Socket;
@@ -15,7 +16,36 @@ public class FileHandler {
      *
      * @param args no args required
      */
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader victimsFile = null;
+        BufferedWriter messagesFile = null;
+        Vector<String> victims = new Vector<>();
+
+        int numberGroup = Integer.parseInt(args[0]);
+        try{
+            victimsFile = new BufferedReader(new FileReader("victims"));
+            messagesFile = new BufferedWriter(new FileWriter("messages"));
+
+            String line;
+
+            while((line = victimsFile.readLine()) != null){
+                victims.add(line);
+            }
+            int x = 0;
+        }catch(IOException e){
+
+        }
+        finally{
+            try{
+                if(victimsFile != null)
+                    victimsFile.close();
+                if(messagesFile != null)
+                    messagesFile.close();
+            }
+            catch(IOException e){
+
+            }
+        }
     }
 }
